7ff86dca2f070e89cb1bcd2419eb068a10e8cd0d
DRINGOT <donovan.ringot@hotmail.fr>
2019-11-12 10:10:22 +0100

    add for loop example

--- Sort/Bubble_sort.js
+++ Sort/Bubble_sort.js
@@ -9,20 +9,41 @@
 //     swapped = true
 // while swapped
 
-// Code:
-let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < len; i++) {
-            if (inputArr[i] > inputArr[i + 1]) {
-                let tmp = inputArr[i];
-                inputArr[i] = inputArr[i + 1];
-                inputArr[i + 1] = tmp;
-                swapped = true;
+const inputArr = [3, 45, 67, 98, 78, 25, 17];
+
+// Code(1):
+// let bubbleSort = (inputArr) => {
+//     let len = inputArr.length;
+//     let swapped;
+//     do {
+//         swapped = false;
+//         for (let i = 0; i < len; i++) {
+//             if (inputArr[i] > inputArr[i + 1]) {
+//                 let tmp = inputArr[i];
+//                 inputArr[i] = inputArr[i + 1];
+//                 inputArr[i + 1] = tmp;
+//                 swapped = true;
+//             }
+//         }
+//     } while (swapped);
+//     return inputArr;
+// };
+
+
+//Code(2):
+let bubbleSort = inputArr => {
+    for (let i = 0; i < inputArr.length - 1; i++) {
+        let change = false;
+        for (let j = 0; j < inputArr.length - (i + 1); j++) {
+            if (inputArr[j] > inputArr[j + 1]) {
+                change = true;
+                [inputArr[j], inputArr[j + 1]] = [inputArr[j + 1], inputArr[j]];
             }
         }
-    } while (swapped);
-    return inputArr;
+        if (!change) break;
+    }
+    return inputArr
 };
+
+bubbleSort(inputArr);
+console.log(inputArr);
